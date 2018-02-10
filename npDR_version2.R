08e289b4ad6e62553fba10b541acdb0284babb43
ainaimi <ashley.naimi@pitt.edu>
2018-02-10 13:32:05 -0500

    Removed unecessary code (previously commented out). Changed coding of SL tuning parameters to avoid dependence on source files.

--- npDR_version2.R
+++ npDR_version2.R
@@ -1,38 +1,34 @@
 # load libraries/functions
 args=(commandArgs(TRUE))
-#setwd(".")
 
 packages <- c("foreach","doParallel","doRNG","boot",
               "rmutil","mvtnorm","gam","sandwich",
               "devtools","glmnet","data.table","rpart",
               "ranger","nnet","arm","earth","e1071","xgboost",
               "foreach")
-#userLib <-  "~/R/R_LIBS_USER"
-#.libPaths(userLib)
-
-#ifelse(!dir.exists(userLib), dir.create(userLib), FALSE)
 
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
-    install.packages(package,repos='http://lib.stat.cmu.edu/R/CRAN') #lib=userLib, 
+    install.packages(package,repos='http://lib.stat.cmu.edu/R/CRAN') 
   }
 }
 
 for (package in packages) {
-  library(package, character.only=T)#, lib.loc=.libPaths()
+  library(package, character.only=T)
 }
 
-devtools::install_github("ecpolley/SuperLearner") #,lib=userLib
-library(SuperLearner)#, lib.loc=.libPaths()
+devtools::install_github("ecpolley/SuperLearner") 
+library(SuperLearner)
 
+# not sure I need rJava, as ck37r is no longer being used
 # https://stackoverflow.com/questions/12872699/error-unable-to-load-installed-packages-just-now/25932828#25932828
-install.packages("rJava",repos='http://lib.stat.cmu.edu/R/CRAN')#lib=userLib, 
-library(rJava)#, lib.loc=.libPaths()
+#install.packages("rJava",repos='http://lib.stat.cmu.edu/R/CRAN') 
+#library(rJava)
 
-devtools::install_github("ck37/ck37r")#,lib=userLib
-library(ck37r)#, lib.loc=.libPaths()
+#devtools::install_github("ck37/ck37r")
+#library(ck37r)
 
-install.packages("tmle",repos='http://lib.stat.cmu.edu/R/CRAN')#lib=userLib,
+install.packages("tmle",repos='http://lib.stat.cmu.edu/R/CRAN')
 library(tmle)
 
 time<-proc.time()
@@ -46,43 +42,39 @@ cols <- c("ipwPMT","ipwNPT","ipwPMF","ipwNPF",
           "regPMT","regNPT","regPMF","regNPF",
           "aipwPMT","aipwNPT","aipwPMF","aipwNPF",
           "tmlePMT","tmleNPT","tmlePMF","tmleNPF")
-res.est <- data.frame(matrix(nrow=1,ncol=length(cols))); #nsim*5
+res.est <- data.frame(matrix(nrow=1,ncol=length(cols)));
 colnames(res.est) <- cols
 res.se <- res.est
 
 ##  true value
-true<-6;true
+true<-3
 
-source("~/Dropbox/SLwrappers/create.SL.glmnet.R")
-source("~/Dropbox/SLwrappers/create.SL.gam.Wrapper.R")
-source("~/Dropbox/SLwrappers/rangerWrapper.R")
-source("~/Dropbox/SLwrappers/rangerWrapper1.R")
-source("~/Dropbox/SLwrappers/rangerWrapper2.R")
-source("~/Dropbox/SLwrappers/rangerWrapper3.R")
-source("~/Dropbox/SLwrappers/rangerWrapper4.R")
-source("~/Dropbox/SLwrappers/rangerWrapper5.R")
-source("~/Dropbox/SLwrappers/create.SL.xgboost.R")
+glmnet_learner = create.Learner("SL.glmnet", tune=list(alpha=seq(0,1,length.out=3))) 
+gam_learner = create.Learner("SL.gam", tune=list(deg.gam=c(2,3,4,5))) 
+ranger_learner = create.Learner("SL.ranger", tune=list(num.trees=c(500,1000),mtry=c(2,3)))
+xgboost_learner = create.Learner("SL.xgboost", tune = list(ntrees = c(500,1000), 
+                                                           max_depth = c(1,2), 
+                                                           shrinkage = c(0.1, 0.001)))
+earth_learner = create.Learner("SL.earth",tune=list(degree=c(1,2),
+                                                    nk=c(4,5),
+                                                    minspan=c(0,1),
+                                                    endspan=c(0,1)))
 
-tune = list(ntrees = c(100, 500), max_depth = c(1, 2), minobspernode = 10,
-            shrinkage = c(0.1, 0.01, 0.001))
-xgb_grid = create.SL.xgboost(tune = tune) #, env = sl_env
-create.SL.glmnet(alpha = c(0,.5)) #setting alpha = 0 gives LASSO, 1 gives elastic net.
-create.SL.gam(deg.gam = c(3,4,5)) 
-sl.lib<-c("SL.glmnet.0","SL.glmnet.0.5","SL.glmnet","SL.rpartPrune",
-          "SL.gam.3","SL.gam.4","SL.gam.5","SL.glm.interaction","SL.earth",
-          "SL.ranger","SL.ranger1","SL.ranger2","SL.ranger3","SL.ranger4","SL.ranger5",
-          "SL.bayesglm","SL.xgboost","SL.mean",xgb_grid$names)
+sl.lib<-c(glmnet_learner$names,"SL.rpartPrune",
+          gam_learner$names,"SL.glm.interaction",earth_learner$names,
+          ranger_learner$names,xgboost_learner$names,
+          "SL.bayesglm","SL.mean")
 
 npDR<-function(counter,bs=F,bootNum=100){
   # data management
   i<-counter
   samp<-rep(1:5,nsim*5)[counter]
-  ss<-c(100,200,600,1200,2000)
+  ss<-c(100,200,600,1200)
   n<-ss[samp]
   cat("Now running iteration",i,"with a sample size of",n,'\n');flush.console()
   
   # confounders
-  sigma<-matrix(0,nrow=4,ncol=4);diag(sigma)<-4
+  sigma<-matrix(0,nrow=4,ncol=4);diag(sigma)<-8
   x <- rmvnorm(n, mean=rep(0,4), sigma=sigma)
   
   #GGally::ggpairs(data.frame(x))
@@ -96,17 +88,17 @@ npDR<-function(counter,bs=F,bootNum=100){
   #GGally::ggpairs(data.frame(z))
   
   # design matrix for exposure and outcome model
-  dMatT<-model.matrix(as.formula(paste("~(",paste("x[,",1:ncol(x),"]",collapse="+"),")"))) ## removed all interactions
+  dMatT<-model.matrix(as.formula(paste("~(",paste("x[,",1:ncol(x),"]",collapse="+"),")")))
   
-  beta<-c(120,3.5,2.5,-1,3.5) # ,2,2.5,1.5,1.5,1.5,1
-  theta<-c(-2,log(2),log(2.5),log(.5),log(1.5)) # ,log(1.75),log(1.5),log(1.25),log(1.25),log(1.25),log(1.25)
+  beta<-c(120,3,3,3,3) 
+  theta<-c(-2,log(2),log(2),log(2),log(2))
   
   # design matrix for propensity score model
   mu <- dMatT%*%beta
   # propensity score model
   pi <- expit(dMatT%*%theta);r<-rbinom(n,1,pi)
   # outcome model: true expsoure effect = 6
-  y <- r*6 + mu + rnorm(n,0,60)
+  y <- r*true + mu + rnorm(n,0,6)
 
   # induce misspecification
   dMat<-model.matrix(as.formula(paste("~(",paste("z[,",1:ncol(x),"]",collapse="+"),")")))
@@ -124,27 +116,8 @@ npDR<-function(counter,bs=F,bootNum=100){
   
   tmleNPT <- tmle(Y,A,data.frame(W),family="gaussian",Q.SL.library=sl.lib,g.SL.library=sl.lib)
 
-  # setup_parallel_tmle(parallel="multicore",max_cores=20)
-  # set.seed(1, "L'Ecuyer-CMRG")
-  # tmleNPT <- tmle_parallel(Y=Y,A=A,W=data.frame(W),
-  #                          family="gaussian",V=folds,
-  #                          Q.SL.library=sl.lib,g.SL.library=sl.lib)
-
-  # SL_out.monT<-data.table(N=n,model="outcome",t(tmleNPT$Qinit$coef))
-  # SL_exp.monT<-data.table(N=n,model="exposure",t(tmleNPT$g$coef))
-  # 
-  # if(i==1&samp==1){
-  #   write.table(SL_out.monT,"SL_outDat_True.txt",sep="\t",row.names=F)
-  #   write.table(SL_exp.monT,"SL_expDat_True.txt",sep="\t",row.names=F)
-  # } else{
-  #   write.table(SL_out.monT,"SL_outDat_True.txt",sep="\t",row.names=F,col.names=F,append=T)
-  #   write.table(SL_exp.monT,"SL_expDat_True.txt",sep="\t",row.names=F,col.names=F,append=T)
-  # }
-  
   pihatPT <- tmlePMT$g$g1W
-  #swPT<-dat$r*(mean(dat$r)/tmlePMT$g$g1W) + (1-dat$r)*((1-mean(dat$r))/(1-tmlePMT$g$g1W))
   pihatNPT <- tmleNPT$g$g1W
-  #swNPT<-dat$r*(mean(dat$r)/tmleNPT$g$g1W) + (1-dat$r)*((1-mean(dat$r))/(1-tmleNPT$g$g1W))
   muhatPT  <- tmlePMT$Qinit$Q[,2]*A+tmlePMT$Qinit$Q[,1]*(1-A)
   muhatPT1 <- tmlePMT$Qinit$Q[,2];muhatPT0 <- tmlePMT$Qinit$Q[,1]
   muhatNPT  <- tmleNPT$Qinit$Q[,2]*A+tmleNPT$Qinit$Q[,1]*(1-A)
@@ -160,21 +133,8 @@ npDR<-function(counter,bs=F,bootNum=100){
   folds<-c(2,2,3,5,5)[samp]
   tmleNPF <- tmle(Y,A,data.frame(W),family="gaussian",Q.SL.library=sl.lib,g.SL.library=sl.lib)
   
-  # SL_out.monF<-data.table(N=n,model="outcome",t(tmleNPF$Qinit$coef))
-  # SL_exp.monF<-data.table(N=n,model="exposure",t(tmleNPF$g$coef))
-  # 
-  # if(i==1&samp==1){
-  #   write.table(SL_out.monF,"SL_outDat_MisSpec.txt",sep="\t",row.names=F)
-  #   write.table(SL_exp.monF,"SL_expDat_MisSpec.txt",sep="\t",row.names=F)
-  # } else{
-  #   write.table(SL_out.monF,"SL_outDat_MisSpec.txt",sep="\t",row.names=F,col.names=F,append=T)
-  #   write.table(SL_exp.monF,"SL_expDat_MisSpec.txt",sep="\t",row.names=F,col.names=F,append=T)
-  # }
-  
   pihatPF <- tmlePMF$g$g1W
-  #swPF<-dat$r*(mean(dat$r)/tmlePMF$g$g1W) + (1-dat$r)*((1-mean(dat$r))/(1-tmlePMF$g$g1W))
   pihatNPF <- tmleNPF$g$g1W
-  #swNPF<-dat$r*(mean(dat$r)/tmleNPF$g$g1W) + (1-dat$r)*((1-mean(dat$r))/(1-tmleNPF$g$g1W))
   muhatPF  <- tmlePMF$Qinit$Q[,2]*A+tmlePMF$Qinit$Q[,1]*(1-A)
   muhatPF1 <- tmlePMF$Qinit$Q[,2];muhatPF0 <- tmlePMF$Qinit$Q[,1]
   muhatNPF  <- tmleNPF$Qinit$Q[,2]*A+tmleNPF$Qinit$Q[,1]*(1-A)
@@ -204,8 +164,6 @@ npDR<-function(counter,bs=F,bootNum=100){
   res.est$tmleNPF<-tmleNPF$estimates$ATE$psi
   
   # compute closed-form SEs
-  #mod1<-lm(y~r,data=dat,weights=swPT)
-  #mod2<-lm(y~r,data=dat,weights=swNPT)
   res.se$ipwPMT <- sd((dat$r*dat$y/pihatPT)/sum(dat$r/pihatPT) -  ((1-dat$r)*dat$y/(1-pihatPT))/sum((1-dat$r)/(1-pihatPT)))
   res.se$ipwNPT <- sd((dat$r*dat$y/pihatNPT)/sum(dat$r/pihatNPT) -  ((1-dat$r)*dat$y/(1-pihatNPT))/sum((1-dat$r)/(1-pihatNPT)))
   res.se$aipwPMT <- sd((((2*dat$r-1)*(dat$y - muhatPT))/((2*dat$r-1)*pihatPT + (1-dat$r)) + muhatPT1 - muhatPT0))
@@ -213,8 +171,6 @@ npDR<-function(counter,bs=F,bootNum=100){
   res.se$tmlePMT <- sqrt(tmlePMT$estimates$ATE$var.psi)*sqrt(n)
   res.se$tmleNPT <- sqrt(tmleNPT$estimates$ATE$var.psi)*sqrt(n)
   
-  #mod1<-lm(y~r,data=dat,weights=swPF)
-  #mod2<-lm(y~r,data=dat,weights=swNPF)
   res.se$ipwPMF <- sd((dat$r*dat$y/pihatPF)/sum(dat$r/pihatPF) -  ((1-dat$r)*dat$y/(1-pihatPF))/sum((1-dat$r)/(1-pihatPF)))
   res.se$ipwNPF <- sd((dat$r*dat$y/pihatNPF)/sum(dat$r/pihatNPF) -  ((1-dat$r)*dat$y/(1-pihatNPF))/sum((1-dat$r)/(1-pihatNPF)))
   res.se$aipwPMF <- sd((((2*dat$r-1)*(dat$y - muhatPF))/((2*dat$r-1)*pihatPF + (1-dat$r)) + muhatPF1 - muhatPF0))
@@ -304,38 +260,6 @@ npDR<-function(counter,bs=F,bootNum=100){
   return(tmp)
 }
 
-
-cores<-20 #detectCores()
-#print(cores)
-#results<-lapply(1:(nsim*5), function(x) npDR(x,bs=F))
-results<-mclapply(1:(nsim*5), function(x) npDR(x,bs=F),mc.cores=cores)
-# results<-do.call(rbind,results)
-#write.table(results,"results.txt",sep="\t",row.names=F)
-
-# cl <- makeCluster(cores)
-# registerDoParallel(cl)
-# clusterCall(cl, function(x) .libPaths(x), .libPaths())
-# a <- 5; b <- nsim
-# rng <- RNGseq(a*b, as.numeric(args[[1]]))
-# pak<-c("SuperLearner","tmle","ranger","rpart",
-#        "nnet","arm","earth","e1071","xgboost",
-#        "glmnet","gam","earth","rmutil")
-# obj<-c('res.est','res.se','true')
-# results<-foreach(i=1:a,
-#                  .packages=pak,
-#                  .export=obj) %:% 
-#   foreach(j=1:b,
-#           .packages=pak,
-#           .export=obj,
-#           r=rng[(i-1)*b + 1:b],
-#           .combine='rbind') %dopar% {
-#             rngtools::setRNG(r)
-#             r <- nparDR(i,j,bs=T)
-#             r}
-# stopCluster(cl)
-# results<-do.call(rbind,results)
-# tt<-(proc.time()-time)/60;tt
-# write.table(results,"results.txt",sep="\t",row.names=F)
-# write.table(tt,"time.txt",sep="\t",row.names=F)
-
+cores<-20 
+results<-mclapply(1:(nsim*4), function(x) npDR(x,bs=F),mc.cores=cores)
 proc.time()-time
